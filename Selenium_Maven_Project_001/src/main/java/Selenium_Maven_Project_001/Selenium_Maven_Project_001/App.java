2a21aa45882be596678ba78e8098c66dae045048
priyalchandekar <priyalchandekar11@outlook.com>
2024-01-23 15:58:48 +0530

    maven project

---
+++ Selenium_Maven_Project_001/src/main/java/Selenium_Maven_Project_001/Selenium_Maven_Project_001/App.java
@@ -0,0 +1,13 @@
+package Selenium_Maven_Project_001.Selenium_Maven_Project_001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
