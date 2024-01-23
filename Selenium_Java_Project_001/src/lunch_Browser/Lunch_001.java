e9d146b742fb68b32ec7a30edb1dee56fc344a00
priyalchandekar <priyalchandekar11@outlook.com>
2024-01-23 15:47:48 +0530

    java project

---
+++ Selenium_Java_Project_001/src/lunch_Browser/Lunch_001.java
@@ -0,0 +1,24 @@
+package lunch_Browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Lunch_001 {
+
+	public static void main(String[] args) {
+		
+		
+		// Lunch Browser
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		// or
+		
+		//WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage()
+
+	}
+
+}
